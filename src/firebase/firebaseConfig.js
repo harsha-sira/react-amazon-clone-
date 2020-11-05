747a6e3f4070eb686cee31785a562e3b13f83f5e
Harsha Siriwardana <harshasiriwardana@Harshas-MacBook-Pro.local>
2020-11-05 11:29:51 +0530

    Initial commit-project setup

---
+++ src/firebase/firebaseConfig.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyASpCOmcrrgS11FPcr-IRxvWRm7Uqg4Hyw",
+  authDomain: "clone-a360c.firebaseapp.com",
+  databaseURL: "https://clone-a360c.firebaseio.com",
+  projectId: "clone-a360c",
+  storageBucket: "clone-a360c.appspot.com",
+  messagingSenderId: "569453149170",
+  appId: "1:569453149170:web:3e56a57e2a0e6f6fe2496b",
+  measurementId: "G-EVGD4M7WMP",
+};
