434c5ed4b2be1ca6e7a9c761e85eb42bb78d2319
Harsha Siriwardana <harshasiriwardana@Harshas-MacBook-Pro.local>
2020-11-05 18:38:37 +0530

    enhance home component with dynamic content loading

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,28 +1,25 @@
 import React from "react";
 import "../css/Product.css";
 
-function Product() {
+function Product({ description, price, starRating, image }) {
   return (
     <div className="product">
       <div className="product_info">
-        <p>The product description.</p>
+        <p>{description}</p>
         <p className="product_price">
           <small>$</small>
-          <strong>19.99</strong>
+          <strong>{price}</strong>
         </p>
         <div className="product_rating">
-          <p>⭐</p>
-          <p>⭐</p>
-          <p>⭐</p>
-          <p>⭐</p>
+          {Array(starRating)
+            .fill()
+            .map((_, i) => (
+              <p>⭐</p>
+            ))}
         </div>
       </div>
 
-      <img
-        className="product_image"
-        src="https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2019/July/amazonbasics_260x260._SY608_CB442725065_.jpg"
-        alt=""
-      />
+      <img className="product_image" src={image} alt="" />
 
       <button className="product_add_to_basket_button">Add to Basket</button>
     </div>
