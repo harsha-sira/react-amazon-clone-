a6d274188d3de810304fe9de0a073e79349ca695
Harsha Siriwardana <harshasiriwardana@Harshas-MacBook-Pro.local>
2020-11-05 13:40:33 +0530

    header component build

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,46 @@
 import React from "react";
 import "../css/Header.css";
 
+import SearchIcon from "@material-ui/icons/Search";
+import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
+
 function Header() {
   return (
-    <div>
-      <h1> THis is header</h1>
+    <div className="header">
+      <img
+        className="header_logo"
+        src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+        alt=""
+      />
+
+      <div className="header_search">
+        <input className="header_input" text="" />
+        <SearchIcon className="header_searchIcon" />
+      </div>
+
+      <div className="header_nav">
+        <div className="header_option">
+          <span className="header_optionLineOne">Hello Guest</span>
+          <span className="header_optionLineTwo">Sign In</span>
+        </div>
+
+        <div className="header_option">
+          <span className="header_optionLineOne">Returns</span>
+          <span className="header_optionLineTwo">& Orders</span>
+        </div>
+
+        {/* <div className="header_option">
+          <span className="header_optionLineOne">Your</span>
+          <span className="header_optionLineTwo">Prime</span>
+        </div> */}
+
+        <div className="header_shppingBasket">
+          <ShoppingCartIcon className="shoppingCartIcon" />
+          <span className="header_shoppingBasket_count header_optionLineTwo">
+            0
+          </span>
+        </div>
+      </div>
     </div>
   );
 }
